2376a07798bad40ea339bcec026a52f98229bbc4
Haad <haaddev@gmail.com>
2025-01-31 02:37:37 +0500

    changes

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyA2XpB6Ct70XWkGx68cbaFjRHB2f-IKix0",
-    authDomain: "auth-app-a9cf0.firebaseapp.com",
-    projectId: "auth-app-a9cf0",
-    storageBucket: "auth-app-a9cf0.firebasestorage.app",
-    messagingSenderId: "1090966263769",
-    appId: "1:1090966263769:web:929cec3ff7f62372686bc8"
-  };
- export const app = initializeApp(firebaseConfig);
- export const auth = getAuth(app)
-  export const db = getFirestore(app);
- 
- 
+const firebaseConfig = {
+  apiKey: "AIzaSyA2XpB6Ct70XWkGx68cbaFjRHB2f-IKix0",
+  authDomain: "auth-app-a9cf0.firebaseapp.com",
+  projectId: "auth-app-a9cf0",
+  storageBucket: "auth-app-a9cf0.firebasestorage.app",
+  messagingSenderId: "1090966263769",
+  appId: "1:1090966263769:web:929cec3ff7f62372686bc8",
+};
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- login.js
+++ login.js
@@ -1,31 +1,28 @@
-import {  signInWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { auth } from "./config.js";
 
-let username = document.querySelector('#name')
-let pass = document.querySelector('#password')
-let email = document.querySelector('#email')
-let subBtn = document.querySelector('#submit')
+let username = document.querySelector("#name");
+let pass = document.querySelector("#password");
+let email = document.querySelector("#email");
+let subBtn = document.querySelector("#submit");
 
+subBtn.addEventListener("click", () => {
+  if (!email.value || !password.value) {
+    alert("Please fill the fields");
+    return;
+  }
 
+  signInWithEmailAndPassword(auth, email.value, pass.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      alert("succesfully Loged In");
+      window.location = "./dashbord.html";
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
 
-subBtn.addEventListener('click' , () => {
-    if (!email.value || !password.value){
-        alert('Please fill the fields')
-        return;
-    }
-
-    signInWithEmailAndPassword(auth, email.value, pass.value)
-  .then((userCredential) => {
-    const user = userCredential.user;
-        alert('succesfully Loged In')
-        window.location = './dashbord.html'
-    
-  }).catch((error) => {
-    const errorMessage = error.message;
-    alert(errorMessage)
-  }) 
-  
-    email.value = ''
-    pass.value = ''
-})
-
+  email.value = "";
+  pass.value = "";
+});
