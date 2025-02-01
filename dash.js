aa0f10e22eccc32a84452902f35d73710ffaed04
Haad <haaddev@gmail.com>
2025-02-01 20:14:10 +0500

    button

--- dash.js
+++ dash.js
@@ -1,83 +1,81 @@
-import { onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import {
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { auth, db } from "./config.js";
-import { collection, addDoc, getDocs , deleteDoc  } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 // User check
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
-
   } else {
-   window.location = './login.html'
+    window.location = "./login.html";
   }
 });
 
-let logBtn = document.querySelector('.logout')
-let userinp = document.querySelector('#userinput')
-let tasklist = document.querySelector('#taskList')
-let addBtn = document.querySelector('.addBtn')
+let logBtn = document.querySelector(".logout");
+let userinp = document.querySelector("#userinput");
+let tasklist = document.querySelector("#taskList");
+let addBtn = document.querySelector(".addBtn");
 
 // Logout Function
-logBtn.addEventListener('click' , () => {
-  signOut(auth).then(() => {
-    alert('logout successfully')
-    window.location = './login.html'
-  }).catch((error) => {
-    alert(error)
-  });
-})
-
-
+logBtn.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      alert("logout successfully");
+      window.location = "./login.html";
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
 
 // set data Function
-addBtn.addEventListener('click' , async (evt) =>{
-  evt.preventDefault()
-  if (!userinp.value ){
-    alert('Please enter a task')
-    return; 
+addBtn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  if (!userinp.value) {
+    alert("Please enter a task");
+    return;
   }
 
   try {
     const docRef = await addDoc(collection(db, "todos"), {
       userUid: auth.currentUser.uid,
-      task:userinp.value,
-     Timestamp: new Date(),
+      task: userinp.value,
+      Timestamp: new Date(),
     });
-    userinp.value = ''
+    userinp.value = "";
     console.log("Document written with ID: ", docRef.id);
-getDatafirestore()
+    getDatafirestore();
   } catch (e) {
     console.error("Error adding document: ", e);
   }
-
-
-})
-
+});
 
 // get data Function
-let getDatafirestore = async () =>{
-  let alltodos = [];
+let alltodos = [];
+let getDatafirestore = async () => {
+  alltodos.length = 0;
   const querySnapshot = await getDocs(collection(db, "todos"));
   querySnapshot.forEach((doc) => {
-  alltodos.unshift(doc.data());
-});
-tasklist.innerHTML = ''
-  alltodos.forEach((item) =>{
+    alltodos.unshift(doc.data());
+  });
+  tasklist.innerHTML = "";
+  alltodos.forEach((item) => {
     tasklist.innerHTML += ` 
          <li>
           ${item.task}
           <span>
             <button class="delBtn">Delete</button>
+            <button class="update">Update</button>
           </span>
         </li> `;
-  })
-}
-getDatafirestore()
-
-// delete data Function
-let delBtn = document.querySelector('.delBtn')
-
-delBtn.addEventListener('click', async () =>{
-  await deleteDoc(doc(db, "todos", userUid));
-
-})
-
+  });
+};
+getDatafirestore();
