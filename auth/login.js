a4d42f8c42ddb2f94fae6d1b679d2917d3eebf3f
Haad <haaddev@gmail.com>
2025-02-05 01:22:05 +0500

    changes

--- auth/login.js
+++ auth/login.js
@@ -1,11 +1,26 @@
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { auth } from "../Main/config.js";
+  import {
+    onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+  } else {
+    window.location = '/Dashboard/dashbord.html';
+  }
+});
+
+
+// initialize all variables
 let username = document.querySelector("#name");
 let pass = document.querySelector("#password");
 let email = document.querySelector("#email");
 let subBtn = document.querySelector("#submit");
 
+
+// Sign-up Function
 subBtn.addEventListener("click", () => {
   if (!email.value || !password.value) {
     alert("Please fill the fields");
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-  // Import the functions you need from the SDKs you need
-  import {auth} from './Main/config.js'
-  import {
-    onAuthStateChanged,
-    signOut,
-} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+//   // Import the functions you need from the SDKs you need
+//   import {auth} from './Main/config.js'
+//   import {
+//     onAuthStateChanged,
+// } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-  } else {
-    window.location = "./Dashboard/dashbord.html";
-  }
-});
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//   } else {
+//     window.location = "./Dashboard/dashbord.html";
+//   }
+// });
